f82232bd6285d8fce29139c65979d97ae81e9179
Parker Lewis <parker.lewis14@att.net>
2016-04-08 14:57:34 -0700

    initial setup of homepage with links

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ var express = require("express"),
 	app = express(),
 	methodOverride = require("method-override"),
 	morgan = require("morgan"),
-	bodyParser = require("body-parser");
-
+	bodyParser = require("body-parser"),
+	usersRouter = require("./routes/users");
 require("locus");
-app.set("view engine", "ejs");
+app.set("view engine", "jade");
 app.use(express.static(__dirname + "/public"));
 app.use(morgan("tiny"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
+app.get("/", function(req, res){
+	res.render("statics/home")
+})
 
+// Routes
+app.use("/users", usersRouter);
 
 
 app.get("*", function(req,res){
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require("express");
+	router = express.Router();
+	//knex
+
+router.route("/")
+	// All the posts
+	// .get() 
+
+
+
+
+module.exports = router;
